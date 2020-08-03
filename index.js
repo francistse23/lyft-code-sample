c2d0e6371c919e309c9be0af107b4e51ea2c138d
francistse-realm <wtse.ucsd@gmail.com>
2020-08-03 12:06:53 -0700

    Added middleware to handle invalid route names.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express().use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    // console.log(req);
-    res.status(200).send("Hello world");
-  } catch (err) {
-    throw new Error(err, "Server is not working");
+    res.status(200).send("Server is up and running");
+  } catch (error) {
+    res.status(500).send({ error });
   }
 });
+
+// to handle invalid routes
+app.use((req, res) =>
+  res.status(404).send("Invalid route name. Please check your input")
+);
 
 app.listen(process.env.PORT || 9000, (error) => {
   if (error) throw new Error(error);
