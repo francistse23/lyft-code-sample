18fb3f745e9c8ba2571ee75d6415f935b7467aa7
francistse-realm <wtse.ucsd@gmail.com>
2020-08-03 13:55:05 -0700

    Added helper function to process input.

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express().use(express.json());
 
+// helper function to process string
+// that returns every 3rd letter from the original string
+function stringProcessor(str) {
+  let moddedString = "";
+
+  // start the loop from the first 3rd letter
+  // then increment by 3 to get the next 3rd letter
+  for (let i = 2; i < str.length; i += 3) {
+    moddedString += str[i];
+  }
+
+  return moddedString;
+}
+
 app.get("/", async (req, res) => {
   try {
     res.status(200).json({ msg: "Server is up and running" });
@@ -25,15 +39,18 @@ app.post("/test", async ({ body }, res) => {
 
     // if it doesnt contain the key string to cut, return some msg 400
     if (!body.hasOwnProperty("string_to_cut"))
-      res.status(400).send("\nRequest body is missing the key string_to_cut.");
-
-    // if it has more than 1 key, return some message 400
+      res
+        .status(400)
+        .send("\nRequest body is missing the key 'string_to_cut'.");
 
     // if the value is empty or less than 3 characters, return empty string/some msg? 204 ?
-
-    console.log(body);
-
-    res.status(200);
+    if (Object.values(body)[0].length < 3) {
+      res.send(204).json({ return_string: "" });
+    } else {
+      res
+        .status(200)
+        .json({ return_string: stringProcessor(Object.values(body)[0]) });
+    }
   } catch (error) {
     res.status(500);
   }
