b56b7d730e3210e72088b876eaa07a3e9cedc285
francistse-realm <wtse.ucsd@gmail.com>
2020-08-03 11:55:13 -0700

    Added GET / to server.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express().use(express.json());
 
+app.get("/", async (req, res) => {
+  try {
+    // console.log(req);
+    res.status(200).send("Hello world");
+  } catch (err) {
+    throw new Error(err, "Server is not working");
+  }
+});
+
 app.listen(process.env.PORT || 9000, (error) => {
   if (error) throw new Error(error);
 
